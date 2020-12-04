d31a65c97331457a8e7e1be46748e5db4f09c3cc
Timo Schmidt <schmidt@timo.email>
2020-12-04 15:49:00 +0100

    BMI calculator added

--- calculator.js
+++ calculator.js
@@ -20,6 +20,18 @@ app.post("/", function(req, res){
     res.send("The result is " + result);
 })
 
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function(req, res){
+    var weight = parseFloat(req.body.weight); // decimal number
+    var height = parseFloat(req.body.height);
+    var bmi = weight / Math.pow(height, 2);
+
+    res.send("Your BMI is " + bmi);
+})
+
 app.listen(3000, function(){
     console.log("Server started on port 3000.")
 });
